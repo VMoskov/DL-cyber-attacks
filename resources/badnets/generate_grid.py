57425495e18cf98e255582239ef40b95481d894a
VMoskov <vm54341@fer.hr>
2023-12-31 12:27:18 +0100

    added MNIST backdoor trigger support

--- resources/badnets/generate_grid.py
+++ resources/badnets/generate_grid.py
@@ -3,15 +3,27 @@ import argparse
 from PIL import Image
 
 
-def generate_white_black_grid_image(image_size, square_size, distance_to_right, distance_to_bottom):
-    black_image = np.zeros((image_size, image_size, 3), dtype=np.uint8)
-    # generate grid with white and black squares at right downside corner
-    for i in range(0, square_size):
-        for j in range(0, square_size):
-            if (i + j) % 2 == 0:
-                black_image[image_size - distance_to_bottom - square_size + i, image_size - distance_to_right - square_size + j, :] = 255
-            else:
-                black_image[image_size - distance_to_bottom - square_size + i, image_size - distance_to_right - square_size + j, :] = 1
+def generate_white_black_grid_image(image_size, square_size, distance_to_right, distance_to_bottom, color_channels):
+    if color_channels == 1:
+        # Create a single-channel image (grayscale)
+        black_image = np.zeros((image_size, image_size), dtype=np.uint8)
+        for i in range(0, square_size):
+            for j in range(0, square_size):
+                if (i + j) % 2 == 0:
+                    black_image[image_size - distance_to_bottom - square_size + i,
+                                image_size - distance_to_right - square_size + j] = 255
+    else:
+        # Create a color image with white and black squares
+        black_image = np.zeros((image_size, image_size, color_channels), dtype=np.uint8)
+        for i in range(0, square_size):
+            for j in range(0, square_size):
+                if (i + j) % 2 == 0:
+                    black_image[image_size - distance_to_bottom - square_size + i,
+                                image_size - distance_to_right - square_size + j, :] = 255
+                else:
+                    black_image[image_size - distance_to_bottom - square_size + i,
+                                image_size - distance_to_right - square_size + j, :] = 0
+
     return black_image
 
 
@@ -21,12 +33,16 @@ if __name__ == '__main__':
     args.add_argument('--square_size', type=int, default=3)
     args.add_argument('--distance_to_right', type=int, default=0)
     args.add_argument('--distance_to_bottom', type=int, default=0)
+    args.add_argument('--color_channels', type=int, default=3)
     args.add_argument('--output_path', type=str, default='./trigger_image_grid.png')
     args = args.parse_args()
+
     image = generate_white_black_grid_image(
         args.image_size,
         args.square_size,
         args.distance_to_right,
         args.distance_to_bottom,
+        args.color_channels,
     )
-    Image.fromarray(image).save(args.output_path)
+    mode = 'RGB' if args.color_channels == 3 else 'L'
+    Image.fromarray(image, mode=mode).save(args.output_path)
--- resources/badnets/generate_square.py
+++ resources/badnets/generate_square.py
@@ -2,12 +2,19 @@ import numpy as np
 import argparse
 from PIL import Image
 
-
-def generate_white_square_image(image_size, square_size, distance_to_right, distance_to_bottom):
-    black_image = np.zeros((image_size, image_size, 3), dtype=np.uint8)
-    black_image[image_size - distance_to_bottom - square_size:image_size - distance_to_bottom, image_size - distance_to_right - square_size:image_size - distance_to_right, :] = 255
+def generate_white_square_image(image_size, square_size, distance_to_right, distance_to_bottom, color_channels):
+    if color_channels == 1:
+        # Create a single-channel image (grayscale)
+        black_image = np.zeros((image_size, image_size), dtype=np.uint8)
+        black_image[image_size - distance_to_bottom - square_size:image_size - distance_to_bottom,
+                    image_size - distance_to_right - square_size:image_size - distance_to_right] = 255
+    else:
+        # Create a color image with white square
+        black_image = np.zeros((image_size, image_size, color_channels), dtype=np.uint8)
+        white_value = [255] * color_channels
+        black_image[image_size - distance_to_bottom - square_size:image_size - distance_to_bottom,
+                    image_size - distance_to_right - square_size:image_size - distance_to_right] = white_value
     return black_image
-
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -15,12 +22,16 @@ if __name__ == '__main__':
     args.add_argument('--square_size', type=int, default=3)
     args.add_argument('--distance_to_right', type=int, default=0)
     args.add_argument('--distance_to_bottom', type=int, default=0)
+    args.add_argument('--color_channels', type=int, default=3)
     args.add_argument('--output_path', type=str, default='./trigger_image.png')
     args = args.parse_args()
+
     image = generate_white_square_image(
         args.image_size,
         args.square_size,
         args.distance_to_right,
         args.distance_to_bottom,
+        args.color_channels,
     )
-    Image.fromarray(image).save(args.output_path)
+    mode = 'RGB' if args.color_channels == 3 else 'L'
+    Image.fromarray(image, mode=mode).save(args.output_path)
