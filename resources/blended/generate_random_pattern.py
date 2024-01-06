8db23854dd7f0c5a46c0dd08c455929484beb32d
VMoskov <vm54341@fer.hr>
2024-01-06 12:43:18 +0100

    modified the script for generating the pattern

--- resources/blended/generate_random_pattern.py
+++ resources/blended/generate_random_pattern.py
@@ -5,16 +5,14 @@ import random
 
 
 def generate_random_pattern_image(color_channels):
-    return np.random.randint(0, 256, (32, 32, color_channels), dtype=np.uint8)
+    return np.random.randint(0, 256, (32, 32, 3), dtype=np.uint8)
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument('--color_channels', type=int, default=3)
     args.add_argument('--output_path', type=str, default='./trigger_image_random.png')
     args = args.parse_args()
 
     image = generate_random_pattern_image(args.color_channels)
 
-    mode = 'RGB' if args.color_channels == 3 else 'L'
-    Image.fromarray(image, mode=mode).save(args.output_path)
+    Image.fromarray(image, mode="RGB").save(args.output_path)
